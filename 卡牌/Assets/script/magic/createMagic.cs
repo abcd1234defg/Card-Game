dfdcecb1472bd883c8b92c1f55cc0f9a094d463e
GoushengLee <luoyangligousheng@gmail.com>
2024-05-28 02:52:33 +0800

    Squashed commit of the following:
    commit 1ec737ae3fc8cc7d12681e059d096681847d47b9
    Author: abcd1234defg <litianyi0413@163.com>
    Date:   Mon May 27 17:43:15 2024 +0100
    
        drawMagic

--- 卡牌/Assets/script/magic/createMagic.cs
+++ 卡牌/Assets/script/magic/createMagic.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class createMagic : MonoBehaviour
@@ -10,10 +11,14 @@ public class createMagic : MonoBehaviour
     public int createNum, maxNum, existNum, drawNum;
     int choice;
     public bool draw;
-   
+    public TextAsset Database;
+    public List<string> LeftCardlist = new List<string>();
+    string[] Carddata;
     // Start is called before the first frame update
     void Start()
     {
+        Carddata = Database.text.Split("\n");
+        LeftCardlist = Carddata.ToList();
         canCreate = true;
         createNum = 2;
         maxNum = 4;
@@ -56,9 +61,26 @@ public class createMagic : MonoBehaviour
 
     void create()
     {
-        for (int i = 0; i < createNum; i++)
+        for(int i = 0; i < createNum; i++)
         {
-            choice = Random.Range(1, 8);
+            int randomIndex = Random.Range(0, LeftCardlist.Count);
+            string selectedCard = LeftCardlist[randomIndex];
+            string cardName = selectedCard.Trim();
+            if (cardName == null)
+            {
+                Debug.Log($"无效的卡牌信息:" + selectedCard);
+                LeftCardlist.RemoveAt(randomIndex);
+                continue;
+            }
+            GameObject cardObject = null;
+            GameObject prefab = Resources.Load<GameObject>(cardName);
+            cardObject = Instantiate(prefab, transform);
+            cardObject.GetComponent<magicCard>().information = selectedCard;
+            LeftCardlist.Remove(selectedCard);
+            
+        }
+    }
+    /*choice = Random.Range(1, 8);
             if(choice == 1)
             {
                 Instantiate(Swap, transform);
@@ -93,7 +115,5 @@ public class createMagic : MonoBehaviour
             {
                 Instantiate(doubleD, transform);
                 existNum++;
-            }
-        }
-    }
+            }*/
 }

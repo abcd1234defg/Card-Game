2b56de6f1f027f0374d6af64b77736463a28e3ee
isotope <wyz1214l@163.com>
2024-06-08 16:02:37 +0800

    new

--- 卡牌/Assets/script/WYZ/EndSC.cs
+++ 卡牌/Assets/script/WYZ/EndSC.cs
@@ -8,7 +8,8 @@ public class EndSC : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
+
     }
 
     // Update is called once per frame
